c7f38ff076326dcfb82c915609f64a2d93afe21d
drizh2 <drizh2@ukr.net>
2024-06-30 13:45:54 +0300

    Set up gateway. Added all microservices to consul. Added Google Oauth. Added profile endpoint. Created docker images for all microservices.

---
+++ music-reviewer-react/src/app/containers/App.jsx
@@ -0,0 +1,152 @@
+import React, {useEffect, useState} from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import {
+  Provider,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import { addAxiosInterceptors } from 'misc/requests';
+import * as pages from 'constants/pages';
+import AuthoritiesProvider from 'misc/providers/AuthoritiesProvider';
+import DefaultPage from 'pageProviders/Default';
+import Loading from 'components/Loading';
+import LoginPage from 'pageProviders/Login';
+import PageContainer from 'pageProviders/components/PageContainer';
+import pageURLs from 'constants/pagesURLs';
+import SecretPage from 'pageProviders/Secret';
+import ListPage from "pageProviders/List";
+import ThemeProvider from 'misc/providers/ThemeProvider';
+import UserProvider from 'misc/providers/UserProvider';
+
+import actionsUser from '../actions/user';
+import actionSongs from '../actions/song';
+import Header from '../components/Header';
+import IntlProvider from '../components/IntlProvider';
+import MissedPage from '../components/MissedPage';
+import SearchParamsConfigurator from '../components/SearchParamsConfigurator';
+import configureStore from "../../misc/redux/configureStore";
+import SongReducer from "../reducers/song";
+import EditPage from "../../pageProviders/Edit";
+
+function App() {
+  const dispatch = useDispatch();
+  const [state, setState] = useState({
+    componentDidMount: false,
+  });
+
+  const messageStore = configureStore(SongReducer)
+
+  const {
+    errors,
+    isFailedSignIn,
+    isFailedSignUp,
+    isFetchingSignIn,
+    isFetchingSignUp,
+    isFetchingUser,
+  } = useSelector(({ user }) => user);
+
+  useEffect(() => {
+    addAxiosInterceptors({
+      onSignOut: () => dispatch(actionsUser.fetchSignOut()),
+    });
+    dispatch(actionsUser.fetchUser());
+    dispatch(actionSongs.fetchSongs());
+    setState({
+      ...state,
+      componentDidMount: true,
+    });
+  }, []);
+
+  return (
+    <UserProvider>
+      <AuthoritiesProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <SearchParamsConfigurator />
+            {/* This is needed to let first render passed for App's
+              * configuration process will be finished (e.g. locationQuery
+              * initializing) */}
+            {state.componentDidMount && (
+              <IntlProvider>
+                <Header onLogout={() => dispatch(actionsUser.fetchSignOut())} />
+                {isFetchingUser && (
+                  <PageContainer>
+                    <Loading />
+                  </PageContainer>
+                )}
+                {!isFetchingUser && (
+                  <Routes>
+                    <Route
+                      element={<DefaultPage />}
+                      path={`${pageURLs[pages.defaultPage]}`}
+                    />
+                    <Route
+                      element={<SecretPage />}
+                      path={`${pageURLs[pages.secretPage]}`}
+                    />
+                    <Route
+                        element={ <ListPage /> }
+                        path={`${pageURLs[pages.list]}`}
+                    />
+                    <Route
+                        element={ <EditPage /> }
+                        path={`${pageURLs[pages.edit]}`}
+                    />
+                    <Route
+                      element={(
+                        <LoginPage
+                          errors={errors}
+                          isFailedSignIn={isFailedSignIn}
+                          isFailedSignUp={isFailedSignUp}
+                          isFetchingSignIn={isFetchingSignIn}
+                          isFetchingSignUp={isFetchingSignUp}
+                          onSignIn={({
+                            email,
+                            login,
+                            password,
+                          }) => dispatch(actionsUser.fetchSignIn({
+                            email,
+                            login,
+                            password,
+                          }))}
+                          onSignUp={({
+                            email,
+                            firstName,
+                            lastName,
+                            login,
+                            password,
+                          }) => dispatch(actionsUser.fetchSignUp({
+                            email,
+                            firstName,
+                            lastName,
+                            login,
+                            password,
+                          }))}
+                        />
+                      )}
+                      path={`${pageURLs[pages.login]}`}
+                    />
+                    <Route
+                      element={(
+                        <MissedPage
+                          redirectPage={`${pageURLs[pages.defaultPage]}`}
+                        />
+                      )}
+                      path="*"
+                    />
+                  </Routes>
+                )}
+              </IntlProvider>
+            )}
+          </BrowserRouter>
+        </ThemeProvider>
+      </AuthoritiesProvider>
+    </UserProvider>
+  );
+}
+
+export default App;
---
+++ music-reviewer-react/src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  // Services
+  USERS_SERVICE: 'http://localhost:3000',
+  BE_URL: "http://localhost:8080",
+  UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
+};
+
+export default config;
